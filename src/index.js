7c1e6e3e213a4d9ea2a7c4ff01781befeb5e8eba
Vanya1000 <Vanya1000@yandex.ru>
2021-12-24 20:08:57 +0300

    refactor: change structure of the project

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 module.exports = function toReadable (number) {
+	let num = ('' + number).split('');//?
 	let twelve = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
 	let ninety = ['thir', 'four', 'fif', 'six', 'seven', 'eigh', 'nine'];
-	let otTwenty = ['twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine']
+	let fromTwelve = ['twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine']
 	let teen = 'teen'
 	let ty = 'ty';
 	let hundred = 'hundred';
-	if (number < 13) {
-		return twelve[number];
-	}
-	if (number < 20) {
-		return ninety[number - 13] + teen;
-	}
-	if (number <= 99) {
-		return ('' + number)[1] == 0 ? otTwenty[('' + number)[0] - 2] + ty : otTwenty[('' + number)[0] - 2] + ty + ' ' + twelve[('' + number)[1]];
-	}
-	if (number == 100) {
-		return 'one hundred';
-	}
-	if (number > 100) {
-		let first = twelve[('' + number)[0]] + ' ' + hundred;
-		let outer = toReadable(+(('' + number)[1] + ('' + number)[2]));
-		return ('' + number)[1] == 0 && ('' + number)[2] == 0 ? first : first + ' ' + outer;
+	switch (num.length) {
+		case 1:
+			return twelve[number];
+		case 2:
+			if (number < 13) {
+				return twelve[number];
+			} else if (number < 20) {
+				return ninety[number - 13] + teen;
+			} else if (number <= 99) {
+				return num[1] == 0 ? `${fromTwelve[num[0] - 2]}${ty}` : `${fromTwelve[num[0] - 2]}${ty} ${twelve[num[1]]}`;
+			}
+		case 3:
+			let first = `${twelve[num[0]]} ${hundred}`;
+			let outer = toReadable(+(num[1] + num[2]));
+			return num[1] == 0 && num[2] == 0 ? first : `${first} ${outer}`;
+		default:
+			return 'enter valid number';
 	}
 }
