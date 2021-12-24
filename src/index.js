85b2f2298beeddc086e179bb7d13f1809d31f200
Vanya1000 <Vanya1000@yandex.ru>
2021-12-24 19:21:49 +0300

    add: relase v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+	let twelve = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
+	let ninety = ['thir', 'four', 'fif', 'six', 'seven', 'eigh', 'nine'];
+	let otTwenty = ['twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine']
+	let teen = 'teen'
+	let ty = 'ty';
+	let hundred = 'hundred';
+	if (number < 13) {
+		return twelve[number];
+	}
+	if (number < 20) {
+		return ninety[number - 13] + teen;
+	}
+	if (number <= 99) {
+		return ('' + number)[1] == 0 ? otTwenty[('' + number)[0] - 2] + ty : otTwenty[('' + number)[0] - 2] + ty + ' ' + twelve[('' + number)[1]];
+	}
+	if (number == 100) {
+		return 'one hundred';
+	}
+	if (number > 100) {
+		let first = twelve[('' + number)[0]] + ' ' + hundred;
+		let outer = toReadable(+(('' + number)[1] + ('' + number)[2]));
+		return ('' + number)[1] == 0 && ('' + number)[2] == 0 ? first : first + ' ' + outer;
+	}
 }
